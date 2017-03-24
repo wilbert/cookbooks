5b93f7f09d3c01eb55a67ba5ea8e7f4c6788cc94
Wilbert Ribeiro <wilbert.k.ribeiro.-nd@espn.com>
2017-03-23 23:32:55 -0300

    Update shoryuken.rb
    Put process in background after run.

--- ruby_application/recipes/shoryuken.rb
+++ ruby_application/recipes/shoryuken.rb
@@ -1,4 +1,4 @@
-execute "bundle exec shoryuken -C config/shoryuken.yml -L log/shoryuken.log --rails" do
+execute "bundle exec shoryuken --rails -R -C config/shoryuken.yml 2>&1 >> log/shoryuken.log &" do
   cwd current_path
   environment config['environment_variables']
 end
